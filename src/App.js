85a514fc2ee40f815e157aa82203ae78e8122ccb
Anders Kökeritz <anders.kokeritz@tyrens.se>
2020-12-08 15:47:17 +0100

    Background

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Day from './Day.js';
 import './App.css';
-
+import background from './bakgrund.jpg';
 
 function App() {
   return (
     <div>
+      <img src={background} style={{width:'100%'}} alt=''/>
       { Array(25).fill(0).map( (_,ix) => <Day day={ix+1} key={ix}/> ) }
+      <div style={{position: 'absolute', right: '12vw', bottom: '10vh', height: '10vh', width: '20vw'}}>
+        An AdventOfCode exercise by Anders Kökeritz
+        </div>
     </div>
   );
 }
