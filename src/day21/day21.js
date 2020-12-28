d9f5a2eede9405baa5c07c763ba368531e6354d0
Anders Kökeritz <anders.kokeritz@gmail.com>
2020-12-28 17:34:23 +0100

    Day 21 & 22

--- src/day21/day21.js
+++ src/day21/day21.js
@@ -6,17 +6,71 @@ import React, {useState, useEffect} from 'react'
 export default function Day21(props) {
     const [solution1, setSolution1] = useState('Solving...');
     const [solution2, setSolution2] = useState('Solving...');
-
     
     useEffect( () => {
-        function solve1(lines) {
+        function solve1and2(lines) {
             console.log('Solve 1');
+            
+            let possibleIngredients = {};
+            let allIngredients = [];
+            for (let line of lines) {
+                let p = line.match(/([\w ]+)\(contains ([\w+, ]+)/);
+                if (!p) continue;
+                allIngredients.push(...(p[1].trim().split(' ')));
+                for (let allergen of p[2].split(',').map(x => x.trim())) {
+                    if (possibleIngredients[allergen]) {
+                        let ingredients = [];
+                        for (let ingr of p[1].trim().split(' ')) {
+                            if (possibleIngredients[allergen].includes(ingr)) {
+                                ingredients.push(ingr);
+                            }
+                        }
+                        possibleIngredients[allergen] = ingredients;
+                    } else {
+                        possibleIngredients[allergen] = p[1].trim().split(' ');
+                    }
+                }
+            }
+            let allergenics = Object.entries(possibleIngredients).map(x => x[1]).flat();
+            let nonAllergenics = allIngredients.filter(x => !allergenics.includes(x));
+
+            console.log('Possible ingredients: ', possibleIngredients);
+            console.log ('All ingredients: ', allIngredients);
+            console.log ('Allergencis ', allergenics);
+            console.log('Non allergenics: ', nonAllergenics);
+            setSolution1(nonAllergenics.length);
+
+
+            let ingredientsList = Object.entries(possibleIngredients);
+
+            let removedOne = true;
+            while (removedOne) {
+                removedOne = false;
+                ingredientsList = ingredientsList.sort((a,b) => a[1].length - b[1].length);
+                for (let i = 0; i< ingredientsList.length; i++ ) {
+                    if (ingredientsList[i][1].length !== 1) {
+                        console.log('Problem detected', ingredientsList[i])
+                    } else {
+                        for (let j = i+1; j< ingredientsList.length; j++) {
+                            let indexToRemove = ingredientsList[j][1].indexOf(ingredientsList[i][1][0]);
+                            if (indexToRemove !== -1) {
+                                ingredientsList[j][1].splice(indexToRemove,1);
+                                removedOne = true;
+                            }
+                        }
+                    }
+                }
+            }
+            console.log ('Actual ingredients/allergens ', ingredientsList);
+            let answer = ingredientsList.sort( (a,b) => a[0].localeCompare(b[0])).map(x => x[1][0]).join(',');
+            console.log ('Canonical ingredients list: '+answer);
+            setSolution2(answer);
         }
 
         fetch(inputData)
         .then(r => r.text())
         .then(t => String(t).split('\n').map(r => r.trim())) 
-        .then(t => solve1(t));
+        .then(t => solve1and2(t));
     }, []);
     
     return (
--- src/day22/day22.js
+++ src/day22/day22.js
@@ -9,14 +9,91 @@ export default function Day22(props) {
 
     
     useEffect( () => {
+        function readDeck(lines) {
+            let deck = [];
+            for (let line of lines) {
+                let card = parseInt(line);
+                if (card) deck.push(card);
+            }
+            return deck;
+        }
+        function play(deck1, deck2, recursive) {
+            console.log('Play', deck1, deck2);
+            let deckHistory = new Set();
+            deckHistory.addDecks = function (deck1, deck2) { 
+                let decks = deck1.join(',')+':'+deck2.join(','); 
+                if (deckHistory.has(decks)) return true;
+                this.add(decks);
+                return false;
+            }
+            let round = 1;
+            while (deck1.length && deck2.length) {
+                // console.log('---'+round+'---', deck1, deck2);
+                if (deckHistory.addDecks(deck1, deck2)) {
+                    console.log('Repeat scenario detected');
+                    return 1;
+                }
+                let card1 = deck1.shift();
+                let card2 = deck2.shift();
+                let winner;
+                if (recursive && deck1.length >= card1 && deck2.length >= card2) {
+                    winner = play (deck1.slice(0,card1), deck2.slice(0,card2));
+                } else {
+                    winner = card1 > card2 ? 1 : 2;
+                }
+                if (winner === 1) {
+                    deck1.push(card1, card2);
+                } else {
+                    deck2.push(card2, card1);
+                }
+                round++;
+            }
+            console.log ('---> winner: '+(deck1.length?1:2))
+            return deck1.length?1:2;
+        }
+        function score (deck) {
+            let score = 0;
+            for (let i=0; i< deck.length; i++) {
+                score += (deck.length-i)*deck[i];
+            }
+            return score;
+        }
+
         function solve1(lines) {
             console.log('Solve 1');
+
+            let player1Deck = readDeck(lines.slice(1,lines.indexOf('')));
+            let player2Deck = readDeck(lines.slice(lines.indexOf('')+1));
+            console.log(player1Deck, player2Deck);
+
+            play(player1Deck, player2Deck);
+            let s = score(player1Deck) + score(player2Deck);
+            console.log(s);
+            setSolution1(s);
+        }
+
+        function solve2(lines) {
+
+            console.log('Solve 2');
+
+            let player1Deck = readDeck(lines.slice(1,lines.indexOf('')));
+            let player2Deck = readDeck(lines.slice(lines.indexOf('')+1));
+            let winner = play(player1Deck, player2Deck, true);
+            let s = score(player1Deck) + score(player2Deck);
+            console.log('The winner is, no'+winner+' with score: '+s);
+            setSolution2(s);
         }
 
         fetch(inputData)
         .then(r => r.text())
         .then(t => String(t).split('\n').map(r => r.trim())) 
         .then(t => solve1(t));
+
+        fetch(inputData)
+        .then(r => r.text())
+        .then(t => String(t).split('\n').map(r => r.trim())) 
+        .then(t => solve2(t));
+
     }, []);
     
     return (
